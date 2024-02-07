8a3ae8d7b9022257339faa7f4e8f46ea4891e4c9
sebabellucci <sebastian.bellucci@gmail.com>
2024-02-07 08:22:16 -0300

    fix error from install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ sudo apt install nala -y
 sudo nala install nmap pulseaudio pavucontrol pcmanfm alsa-utils mpv xdg-user-dirs alacritty suckless-tools xorg feh nitrogen neofetch -y
 sudo nala install unzip wget pavucontrol build-essential psmisc mangohud papirus-icon-theme fonts-noto-color-emoji slim -y
 sudo nala install flameshot policykit-1 blueman terminator nm-tray nm-tray-l10n thunar arandr gdebi curl htop wget build-essential devscripts software-properties-common -y
-sudo nala install bspwm sxhkd rofi apcalc dunst dunstify picom -y
+sudo nala install bspwm sxhkd rofi apcalc dunst picom -y
 
 xdg-user-dirs-update
 sudo systemctl enable slim.service
@@ -28,8 +28,8 @@ cd $builddir
 mkdir -p ~/.config/.config
 mkdir -p ~/.config/.fonts
 mkdir -p ~/Pictures/backgrounds
-cp -R dotfiles/* /~/.config/
-cp wallpapers/* /~/Pictures/backgrounds/
+cp -R dotfiles/* ~/.config/
+cp wallpapers/* ~/Pictures/backgrounds/
 chown -R $username:$username ~/
 chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/polibar/lauch.sh
