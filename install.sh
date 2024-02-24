41edc1680ca9893d2ac3291cc915c13f01e5d3e2
sebabellucci <sebastian.bellucci@gmail.com>
2024-02-24 03:33:44 -0300

    modified files sxhkdrc and install.sh

--- install.sh
+++ install.sh
@@ -16,10 +16,10 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install nala nmap pulseaudio pavucontrol alsa-utils mpv xdg-user-dirs suckless-tools xorg x11-utils x11-session-utils feh neofetch htop chromium mplayer mpv libfreetype6-dev libimlib2-dev -y
-sudo apt install unzip wget curl build-essential devscripts software-properties-common psmisc mangohud papirus-icon-theme fonts-noto-color-emoji libxft-dev simplescreenrecorder i3lock -y
-sudo apt install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libuv1-dev -y
+sudo apt install unzip wget curl build-essential devscripts software-properties-common psmisc mangohud papirus-icon-theme fonts-noto-color-emoji libxft-dev simplescreenrecorder i3lock xinput redshift -y
+sudo apt install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libuv1-dev cmus -y
 sudo apt install bison flex libstartup-notification0-dev check autotools-dev libpango1.0-dev librsvg2-bin librsvg2-dev libcairo2-dev libglib2.0-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev -y
-sudo apt install flameshot policykit-1 blueman terminator nm-tray nm-tray-l10n thunar arandr gdebi apcalc bspwm sxhkd rofi dunst picom polybar alacritty check slim libpam0g-dev libxrandr-dev -y
+sudo apt install flameshot policykit-1 blueman terminator nm-tray nm-tray-l10n thunar arandr gdebi apcalc bspwm sxhkd rofi dunst picom polybar alacritty check slim libpam0g-dev libxrandr-dev ranger -y
 
 xdg-user-dirs-update
 sudo systemctl enable slim.service
