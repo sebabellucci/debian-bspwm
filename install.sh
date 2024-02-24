625e6f5e13eb0ad0ef90c9b20f9aa63c3a1eff6a
sebabellucci <sebastian.bellucci@gmail.com>
2024-02-24 01:31:48 -0300

    modified files

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ sudo apt upgrade -y
 sudo apt install nala -y
 
 sudo nala install nmap pulseaudio pavucontrol alsa-utils mpv xdg-user-dirs suckless-tools xorg feh neofetch htop chromium mplayer mpv libfreetype6-dev libimlib2-dev -y
-sudo nala install unzip wget curl build-essential devscripts software-properties-common psmisc mangohud papirus-icon-theme fonts-noto-color-emoji libxft-dev -y
+sudo nala install unzip wget curl build-essential devscripts software-properties-common psmisc mangohud papirus-icon-theme fonts-noto-color-emoji libxft-dev simplescreenrecorder i3lock -y
 sudo nala install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libuv1-dev -y
 sudo nala install bison flex libstartup-notification0-dev check autotools-dev libpango1.0-dev librsvg2-bin librsvg2-dev libcairo2-dev libglib2.0-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev -y
 sudo nala install flameshot policykit-1 blueman terminator nm-tray nm-tray-l10n thunar arandr gdebi apcalc bspwm sxhkd rofi dunst picom polybar alacritty check slim libpam0g-dev libxrandr-dev -y
@@ -33,7 +33,7 @@ mkdir -p ~/Pictures/Screenshots
 cp -R dotfiles/* ~/.config/
 cp wallpapers/* ~/Pictures/Backgrounds/
 sudo cp fonts/* /usr/share/fonts/truetype/
-sudo cp dotfiles/slim/slim.conf /etc && sudo cp dotfiles/slim/slimlock.conf /etc
+sudo cp dotfiles/slim/slim.conf /etc
 sudo cp dotfiles/slim/blue-sky /usr/share/slim/themes
 chown -R $username:$username ~/
 rm -r -d ~/.config/slim
