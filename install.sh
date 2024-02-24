261c04ff35e80245f3a2e4a8b686aad54268be4a
sebabellucci <sebastian.bellucci@gmail.com>
2024-02-24 01:15:19 -0300

    modified install.sh

--- install.sh
+++ install.sh
@@ -16,13 +16,11 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install nala -y
 
-sudo nala install nmap pulseaudio pavucontrol alsa-utils mpv xdg-user-dirs suckless-tools xorg feh neofetch htop Chromium mplayer2 mpv -y
-sudo nala install unzip wget curl build-essential devscripts software-properties-common psmisc mangohud papirus-icon-theme fonts-noto-color-emoji -y
-sudo nala install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libuv1-dev -y 
+sudo nala install nmap pulseaudio pavucontrol alsa-utils mpv xdg-user-dirs suckless-tools xorg feh neofetch htop chromium mplayer mpv libfreetype6-dev libimlib2-dev -y
+sudo nala install unzip wget curl build-essential devscripts software-properties-common psmisc mangohud papirus-icon-theme fonts-noto-color-emoji libxft-dev -y
+sudo nala install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libuv1-dev -y
 sudo nala install bison flex libstartup-notification0-dev check autotools-dev libpango1.0-dev librsvg2-bin librsvg2-dev libcairo2-dev libglib2.0-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev -y
-
-sudo nala install flameshot policykit-1 blueman terminator nm-tray nm-tray-l10n thunar arandr gdebi apcalc -y
-sudo nala install bspwm sxhkd rofi dunst picom polybar alacrity check slim slimlock libpam0g-dev libxrandr-dev libfreetype6-dev libimlib2-dev libxft-dev feh -y
+sudo nala install flameshot policykit-1 blueman terminator nm-tray nm-tray-l10n thunar arandr gdebi apcalc bspwm sxhkd rofi dunst picom polybar alacritty check slim libpam0g-dev libxrandr-dev -y
 
 xdg-user-dirs-update
 sudo systemctl enable slim.service
@@ -40,10 +38,10 @@ sudo cp dotfiles/slim/blue-sky /usr/share/slim/themes
 chown -R $username:$username ~/
 rm -r -d ~/.config/slim
 chmod +x ~/.config/bspwm/bspwmrc
-chmod +x ~/.config/polibar/launch.sh
-chmod +x ~/.config/polibar/scripts/launcher
-chmod +x ~/.config/polibar/scripts/powermenu
-chmod +x ~/.config/polibar/scripts/powermenu_alt
+chmod +x ~/.config/polybar/launch.sh
+chmod +x ~/.config/polybar/scripts/launcher
+chmod +x ~/.config/polybar/scripts/powermenu
+chmod +x ~/.config/polybar/scripts/powermenu_alt
 
 cd /tmp
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
